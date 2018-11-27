beaf20a454afa3841930412d8e352d7a9881fc92
Sandy3796 <45390269+Sandy3796@users.noreply.github.com>
2018-11-27 19:45:38 +0530

    Done with First Java

---
+++ welcome.java
@@ -0,0 +1,18 @@
+class samplehello1
+{
+	public void firstmethod()
+	{
+		System.out.print("hello world");
+	}
+}
+class samplehello{
+	public static void main(String[] args) {
+		/*// TODO Auto-generated method stub
+System.out.println("\"HELLO WORLD\"");
+System.out.println((char)34+"SANDHYA"+(char)34);
+System.out.println();*/
+		samplehello1 s1=new samplehello1();
+		s1.firstmethod();
+	}
+
+}
